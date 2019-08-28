29cf6f58a964630e84a1bef151c98b37135d80a7
lokesh <lokeshreddy993@gmail.com>
2019-08-28 11:27:19 -0500

    GitHub

---
+++ demo/src/main/java/com/example/model/PojoClass.java
@@ -0,0 +1,26 @@
+package com.example.model;
+
+public class PojoClass {
+	String Empid;
+	String Empname;
+	
+	public PojoClass(String Empid,String Empname)
+	{
+		this.Empid=Empid;
+		this.Empname=Empname;
+	}
+	
+	public String getEmpid() {
+		return Empid;
+	}
+	public void setEmpid(String empid) {
+		Empid = empid;
+	}
+	public String getEmpname() {
+		return Empname;
+	}
+	public void setEmpname(String empname) {
+		Empname = empname;
+	}
+
+}
